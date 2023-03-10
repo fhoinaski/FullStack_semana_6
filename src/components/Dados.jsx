30830c79a25ca123139614acae68f3a116ecea80
fhoinaski <sukinodoncai@gmail.com>
2023-03-10 19:02:36 -0300

    update

--- src/components/Dados.jsx
+++ src/components/Dados.jsx
@@ -1,7 +1,7 @@
 export const dados = {
     nome: 'Fernando Hoinaski',
     idade: "Idade:37 Anos",
-    foto:'../../public/img/imagemPerfil.png',
+    foto:'https://raw.githubusercontent.com/fhoinaski/FullStack_semana_6/main/public/img/imagemPerfil.png',
     github: 'https://github.com/fhoinaski',
     linkedin: 'https://linkedin.com/in/fhoinaski'
 };
